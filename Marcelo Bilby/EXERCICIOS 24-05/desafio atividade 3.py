d3485ebd365d6d4bbcdcc2da261a175dc59a349d
Marcelo Bilby Barbosa <marcelo.bilby@outlook.com>
2022-05-27 13:34:14 -0400

    Marcelo Bilby - Atualização de Exercício
    Atualização exercício

--- Marcelo Bilby/EXERCICIOS 24-05/desafio atividade 3.py
+++ Marcelo Bilby/EXERCICIOS 24-05/desafio atividade 3.py
@@ -1,8 +1,8 @@
 # IMPORT PACKAGES
 
-from unicodedata import decimal
 import pandas as pd
-from prompt_toolkit import formatted_text
+
+# from prompt_toolkit import formatted_text
 from IPython.display import display
 
 def retornaValorReal(valor):
@@ -13,9 +13,9 @@ def retornaValorReal(valor):
 
 # DATAFRAME
 
-funcionarios_df = pd.read_csv('data/CadastroFuncionarios.csv', sep=';', decimal=',')
-clientes_df = pd.read_csv('data/CadastroClientes.csv', sep=';', decimal=',')
-servicos_df = pd.read_excel('data/BaseServiçosPrestados.xlsx')
+funcionarios_df = pd.read_csv('./data/CadastroFuncionarios.csv', sep=';', decimal=',')
+clientes_df = pd.read_csv('./data/CadastroClientes.csv', sep=';', decimal=',')
+servicos_df = pd.read_excel('./data/BaseServiçosPrestados.xlsx')
 
 # GET SIZE TABLE FUNCIONÁRIOS AND TABLE SERVIÇOS
 
@@ -27,19 +27,16 @@ funcionariosQuePossuemContratos = len(servicos_df["ID Funcionário"].unique())
 percentual = funcionariosQuePossuemContratos/totalDeFuncionarios
 percentual = percentual * 100
 
-print(f"O percentual de funcionários é de: {percentual:.2f} %")
+print(f"O percentual de funcionários que fecharam contrato é de: {percentual:.2f} %")
+print("")
+print("Lista de funcionários que não fecharam contrato: ")
 
 totalFuncionarios = funcionarios_df["ID Funcionário"].unique()
 totalContratos = servicos_df["ID Funcionário"].unique()
 
-filtro = [totalContratos for totalContratos in totalFuncionarios]
+naoFecharamContrato = funcionarios_df.loc[~funcionarios_df["ID Funcionário"].isin(servicos_df["ID Funcionário"])].reset_index(drop=True)
 
-naoFecharamContrato = funcionarios_df.drop(filtro, inplace=True)
 
-print(len(filtro))
-#filtro = funcionarios_df.drop(filtro, axis=0)
-#print(len(filtro))
-print(len(totalFuncionarios))
-print(len(totalContratos))
-#print(len(naoFecharamContrato))
-#display(naoFecharamContrato)
+display(naoFecharamContrato)
+
+
